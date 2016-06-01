b427a1cdff228a10e98ddbf3c6c0d10d9e4760dd
Laure Guery <laureguery@hotmail.com>
2016-06-01 16:34:37 +0100

    add

--- guessWorld.js
+++ guessWorld.js
@@ -1,79 +1,97 @@
 'use strict';
 
-var words = new Array(
-
-    'Javascript',
-    'property',
-    'element',
-    'object';
-    'event',
-    'listener';
-    'array';
-    'statements';
-    'operator';
-    'variable'
-  );
-
-var theWord = "";
-var attempts = 0;
-var hints = 0;
-
-function selectWord(){
-    //random number
-	var ranNum = Math.floor(Math.random()*words.length);
-	theWord = words[ranNum];
-	theWord = theWord.toUpperCase();
-
- 
-
-
-//listening for button click
-var event1 = document.getElementById("button1");
-event1.addEventListener("click" , magicNumber , false);
-   
-//counter 
-var counter = 0;
-
-
-function magicNumber(){
-    //
-    var userinput =document.getElementById("box").value;
-    var number = parseInt(userinput);
-    //var guess = parseInt(randomnumber);
-
-     var text = "";
+play function(){
     
+
+   categories = [
+      ["chrome", "firefox", "codepen", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "banner", "browser", "screen", "mobile", "footer", "header", "typography", "responsive", "programmer", "css", "border", "github", "grunt", "pixel", "document", "object", "ruby", "modernizr", "bootstrap", "python", "php", "pattern", "ajax", "node", "element", "android", "application", "adobe", "apple", "google", "microsoft", "bookmark", "internet", "icon", "svg", "background", "property", "syntax", "flash", "html", "font", "blog", "network", "server", "content", "database", "socket", "function", "variable", "link", "apache", "query", "proxy", "backbone", "angular", "email", "underscore", "cloud"]
+       ["archery","badminton","basketball","karate","boxing,kayak","cycling","diving","equestrian","fencing","fieldhockey","golf","gymnastics","handball","judo","rowing","rugby","sailing","shooting","football","swimming","taekwondo","tennis","triathlon","volleyball","weightlifting","wrestling","athleticism","basketball","baseball"]
+      
+  ["The Godfather","The-Dark-Knight","gladiator","alien", "saw", "jaws","psycho", "le-voyage-dans-la- lune","independence-day ","titanic","batman","spiderman","hulk","captain-america","superman","carol","rocky","jurassic-park","ghostbusters","top-gun","forrest-gump" ,"scarface","goodfellas","braveheart","big","beetlejuice"]
+      ];
     
-    if (number < randomNumber){
-        counter++;
-        text = " Wrong ! your guess is to low. Try again. This is guess no. " + counter;
-        tries();
+    var categories;
+    var chosenCategory;
+    var word;
+    var wrongGuesses=[];
+    var rightGuesses=[];
+    var space, // number of space in word'-'
+        
+        
+        // select Category
+        
+        var  selectCat = function (){
+            if (chosenCategory===categories[0]){
+                categoryName.innerHTML = "The chosen Category is web related";
+            }
+            else if(chosenCategory=== categories[1]){
+                categoryName.innerHTML = " The chosen Category is sport related"
+            }
+            
+            else if (chosenCategory===categories[2]){
+                categoryName.innerHTML= "The chosen Category is film related"
+            }
+            
         }
-       
-        else if (number > randomNumber){
-            counter = counter + 1;
-            text = " Wrong ! your guess is to high. Try again! This is guess no. " + counter;
-        tries();
-        }
-        else {
-            counter++;
-           text = " Congratulations! You find the Magic number. You took " + counter + "guesses.";
-        }
+    chosenCategory = categories[Math.floor(Math.random()*categories.length)];
+    word = chosenCategory[Math.floor(Math.random()*chosenCategory.length)];
+    word = word.replace(/\s/g,"-");
+    console.log(word);
+    buttons();
     
- 
- //document.getElementById("demo").innerHTML = guess;
-    document.getElementById("output").innerHTML = text;
-     //document.getElementById("debug").innerHTML = number;
-} //end of function
-
-
-function tries(){
-  // alert(counter);
-   if (counter>5) {
-       window.alert("GAME OVER!");
-   }
-    
+   //guesses=[];
+    lives =7;
+    counter =0;
+    space =0;
+    result();
+    comments();
+    selectCat();
+    convas();   
 }
+
+// Reset
+
+document.getElementById9('reset').onclick = function(){
+    
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    //context.clearRect (0,0,400,400)// only if want the hangman... to clear a rectangle with a giving rectangle
+    play()
+}
+
+fucntion alterAt(n,c,originalString){
+    return originalString.substr(0,n) + c+ originalString.substr(n+1, originalString.length);
+}
+
+function guessLetter(letter, answer,shown)
+{
+    var checkIndex=0;
+    checkIndex = answer.indexOf(letter);
+    while (checkIndex>=0){
+        shown= alterAt(checkIndex,letter, shown);
+        checkIndex = answer.indexOf(letter, checkIndex + 1);
+    }
+    return shown;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
